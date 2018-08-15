e99fbb484077caf853d59a9441d7b746b032aa7b
Goayandi <r.borutha@gmail.com>
2018-08-15 15:49:38 +0200

    Cappu: Initial changes for Mi Pad 3

---
+++ Android.mk
@@ -0,0 +1,12 @@
+#LOCAL_PATH := $(call my-dir)
+
+#ifneq ($(filter cappu, $(TARGET_DEVICE)),)
+#include $(CLEAR_VARS)
+#LOCAL_MODULE = libdpframework
+#LOCAL_MODULE_CLASS = SHARED_LIBRARIES
+#LOCAL_MODULE_SUFFIX = .so
+#LOCAL_SRC_FILES_32 = proprietary/vendor/lib/libdpframework.so
+#LOCAL_SRC_FILES_64 = proprietary/vendor/lib64/libdpframework.so
+#include $(BUILD_PREBUILT)
+
+#endif
