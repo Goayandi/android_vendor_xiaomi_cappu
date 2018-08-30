3b81a98de2bf69f9572f1a12a3ff587154720a29
goayandi <r.borutha@gmail.com>
2018-08-31 01:57:30 +0200

    Add missing stuff

--- Android.mk
+++ Android.mk
@@ -1,12 +1,36 @@
-#LOCAL_PATH := $(call my-dir)
+LOCAL_PATH := $(call my-dir)
+ifneq ($(filter cappu, $(TARGET_DEVICE)),)
 
-#ifneq ($(filter cappu, $(TARGET_DEVICE)),)
-#include $(CLEAR_VARS)
-#LOCAL_MODULE = libdpframework
-#LOCAL_MODULE_CLASS = SHARED_LIBRARIES
-#LOCAL_MODULE_SUFFIX = .so
-#LOCAL_SRC_FILES_32 = proprietary/vendor/lib/libdpframework.so
-#LOCAL_SRC_FILES_64 = proprietary/vendor/lib64/libdpframework.so
-#include $(BUILD_PREBUILT)
+include $(CLEAR_VARS)
+LOCAL_MODULE := libdpframework
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX = .so
+LOCAL_MULTILIB := 32
+LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libdpframework.so
+include $(BUILD_PREBUILT)
 
-#endif
+include $(CLEAR_VARS)
+LOCAL_MODULE := libdpframework
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX = .so
+LOCAL_MULTILIB := 64
+LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libdpframework.so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libmtkcam_fwkutils
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MULTILIB := 32
+LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libmtkcam_fwkutils.so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libmtkcam_fwkutils
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MULTILIB := 64
+LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libmtkcam_fwkutils.so
+include $(BUILD_PREBUILT)
+
+endif
