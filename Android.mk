030299e59fc7ee8799fbce38949265f26f15f71e
goayandi <r.borutha@gmail.com>
2018-09-12 23:15:33 +0200

    First upload: error while building > > Some errors while building lineage-14.1 and rr-nougat. Looks like something is wrong with lexer (?)

--- Android.mk
+++ Android.mk
@@ -49,25 +49,4 @@ LOCAL_MULTILIB := 64
 LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libmtkcam_fwkutils.so
 include $(BUILD_PREBUILT)
 
-
-include $(CLEAR_VARS)
-LOCAL_MODULE = libstagefright_color_conversion
-LOCAL_MODULE_CLASS = STATIC_LIBRARIES
-LOCAL_MODULE_SUFFIX = .a
-LOCAL_UNINSTALLABLE_MODULE = true
-LOCAL_MULTILIB = 64
-LOCAL_SHARED_LIBRARIES_64 = libdpframework
-LOCAL_SRC_FILES_64 = libstagefright_color_conversion/libstagefright_color_conversion_64.a
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE = libstagefright_color_conversion
-LOCAL_MODULE_CLASS = STATIC_LIBRARIES
-LOCAL_MODULE_SUFFIX = .a
-LOCAL_UNINSTALLABLE_MODULE = true
-LOCAL_MULTILIB = 32
-LOCAL_SHARED_LIBRARIES_32 = libdpframework
-LOCAL_SRC_FILES_32 = libstagefright_color_conversion/libstagefright_color_conversion_32.a
-include $(BUILD_PREBUILT)
-
 endif
