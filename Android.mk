9814463f1c9e809a4ebd04dbb88c856a709eb145
goayandi <r.borutha@gmail.com>
2018-09-23 17:48:04 +0200

    Oreo: initial changes for Mi Pad 3

--- Android.mk
+++ Android.mk
@@ -1,82 +1,77 @@
 LOCAL_PATH := $(call my-dir)
-ifneq ($(filter cappu, $(TARGET_DEVICE)),)
+
+ifeq ($(TARGET_DEVICE),cappu)
+
 include $(CLEAR_VARS)
-LOCAL_MODULE := libaudiocustparam
+LOCAL_MODULE := libhwm
+LOCAL_SRC_FILES_64 := lib64/libhwm.so
+LOCAL_SRC_FILES_32 := lib/libhwm.so
+LOCAL_MULTILIB := both
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
-LOCAL_MULTILIB := 64
-LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libaudiocustparam.so
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := libaudiocustparam
+LOCAL_MODULE := libcustom_prop
+LOCAL_SRC_FILES_64 := lib64/libcustom_prop.so
+LOCAL_SRC_FILES_32 := lib/libcustom_prop.so
+LOCAL_MULTILIB := both
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
-LOCAL_MULTILIB := 32
-LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libaudiocustparam.so
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libdpframework
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX = .so
-LOCAL_MULTILIB := 32
-LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libdpframework.so
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := libdpframework
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX = .so
-LOCAL_MULTILIB := 64
-LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libdpframework.so
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE = libged
-LOCAL_MODULE_CLASS = SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX = .so
-LOCAL_MULTILIB = 32
-LOCAL_SRC_FILES_32 = proprietary/vendor/lib/libged.so
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE = libged
-LOCAL_MODULE_CLASS = SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX = .so
-LOCAL_MULTILIB = 64
-LOCAL_SRC_FILES_64 = proprietary/vendor/lib64/libged.so
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE = libion_mtk
-LOCAL_MODULE_CLASS = SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX = .so
-LOCAL_MULTILIB = 32
-LOCAL_SRC_FILES_32 = proprietary/vendor/lib/libion_mtk.so
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE = libion_mtk
-LOCAL_MODULE_CLASS = SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX = .so
-LOCAL_MULTILIB = 64
-LOCAL_SRC_FILES_64 = proprietary/vendor/lib64/libion_mtk.so
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := libmtkcam_fwkutils
+LOCAL_SRC_FILES_64 := lib64/libdpframework.so
+LOCAL_SRC_FILES_32 := lib/libdpframework.so
+LOCAL_MULTILIB := both
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
-LOCAL_MULTILIB := 32
-LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libmtkcam_fwkutils.so
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := libmtkcam_fwkutils
+LOCAL_MODULE := libged
+LOCAL_SRC_FILES_64 := lib64/libged.so
+LOCAL_SRC_FILES_32 := lib/libged.so
+LOCAL_MULTILIB := both
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
-LOCAL_MULTILIB := 64
-LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libmtkcam_fwkutils.so
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libion_mtk
+LOCAL_SRC_FILES_64 := lib64/libion_mtk.so
+LOCAL_SRC_FILES_32 := lib/libion_mtk.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libnvram
+LOCAL_SRC_FILES_64 := lib64/libnvram.so
+LOCAL_SRC_FILES_32 := lib/libnvram.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := librilmtk
+LOCAL_SRC_FILES_64 := lib64/librilmtk.so
+LOCAL_SRC_FILES_32 := lib/librilmtk.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := mtk-ril
+LOCAL_SRC_FILES_64 := lib64/mtk-ril.so
+LOCAL_SRC_FILES_32 := lib/mtk-ril.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
 endif
